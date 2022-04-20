ec76e40757a00872310ce91ea37a8f9d16c3d0c9
Jeehee77 <gnarled47@gmail.com>
2022-04-20 18:28:28 +0900

    Register - Email 항목 추가

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -97,10 +97,13 @@ const InputP = styled.p`
 `;
 
 const Register = () => {
-  const errorMsg = ['', 'empty username', 'empty password', 'invalid username', 'invalid password', 'incorrect repeat password'];
+  const errorMsg = ['', 'empty email', 'wrong email', 'empty username', 'invalid username',
+    'empty password', 'invalid password', 'incorrect repeat password'];
   const invalidUsername = /[^a-z0-9]/g;
   const invalidPassword = /[+ㄱ-ㅎ가-힣]/g;
+  const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
   const [inputs, setInputs] = useState({
+    email: "",
     username: "",
     password: "",
     repeatPassword: ""
@@ -126,26 +129,45 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     console.log("clicked");
-    if (inputs.username === "") {
+    if (inputs.email === "") {
       setError(1);
-    } else if (inputs.password === "") {
+    } else if (!regEmail.test(inputs.email)) {
       setError(2);
-    } else if (invalidUsername.test(inputs.username) || inputs.username.length > 12 || inputs.username.length < 8) {
+    } else if (inputs.username === "") {
       setError(3);
-    } else if (invalidPassword.test(inputs.password) || inputs.password.length < 8) {
+    } else if (invalidUsername.test(inputs.username) || inputs.username.length > 12 || inputs.username.length < 8) {
       setError(4);
+    } else if (inputs.password === "") {
+      setError(5);
+    } else if (invalidPassword.test(inputs.password) || inputs.password.length < 8) {
+      setError(6);
     } else if (inputs.password !== inputs.repeatPassword) {
       console.log(inputs.password, inputs.repeatPassword);
-      setError(5);
+      setError(7);
     } else {
       console.log('폼 입력 성공');
-      setError(0);
+      
       // 백엔드로 요청 전송
+      // const url = "http://localhost:4000/register";
+      // const payload = {
+      //   email: inputs.email,
+      //   username: inputs.username,
+      //   password: inputs.password
+      // }
+      // axios.post(url, payload)
+      // .then((res) => {
+      //   // 이미 존재하는 회원일 경우, username이 이미 존재할 경우 error 설정
+
+      //   // 회원가입 성공
+      //   setError(0);
+      // })
+      
     }
   }
 
   return (
     <Main>
+      {error === 0 ? <Redirect to="/" /> : null}
       <Section>
         <Container>
           <Div>
@@ -156,31 +178,36 @@ const Register = () => {
           <Div>
             <FormDiv>
               <InputDiv>
+                <h3>Email</h3>
+              </InputDiv>
+              <Input type="email" name="email" placeholder='이메일을 입력해주세요' onChange={handleInputs} />
+              {error === 1 ? (<MsgDiv><Message>{errorMsg[1]}</Message></MsgDiv>) : (
+                error === 2 ? (<MsgDiv><Message>{errorMsg[2]}</Message></MsgDiv>) : null
+              )}
+              <InputDiv>
                 <h3>Username</h3>
                 <InputP>아이디는 6-12자의 영문, 숫자</InputP>
               </InputDiv>
-              <Input type="text" name="username" placeholder='username' onChange={handleInputs} />
+              <Input type="text" name="username" placeholder='username을 입력해주세요' onChange={handleInputs} />
+              {error === 3 ? (<MsgDiv><Message>{errorMsg[3]}</Message></MsgDiv>) : (
+                error === 4 ? (<MsgDiv><Message>{errorMsg[4]}</Message></MsgDiv>) : null
+              )}
               <InputDiv>
                 <h3>Password</h3>
                 <InputP>비밀번호는 8자 이상, 영문/숫자/기호 사용가능</InputP>
               </InputDiv>
-              <Input type="password" name="password" placeholder='password' onChange={handleInputs} />
+              <Input type="password" name="password" placeholder='password를 입력해주세요' onChange={handleInputs} />
+              {error === 5 ? (<MsgDiv><Message>{errorMsg[5]}</Message></MsgDiv>) : (
+                error === 6 ? (<MsgDiv><Message>{errorMsg[6]}</Message></MsgDiv>) : null
+              )}
               <InputDiv>
                 <h3>Repeat Password</h3>
                 <InputP>비밀번호를 재입력해주세요</InputP>
               </InputDiv>
-              <Input type="password" name="repeatPassword" placeholder='repeat password' onChange={handleInputs} />
+              <Input type="password" name="repeatPassword" placeholder='password를 입력해주세요' onChange={handleInputs} />
+              {error === 7 ? (<MsgDiv><Message>{errorMsg[7]}</Message></MsgDiv>) : null}
               <MsgDiv>
-                {error === 1 ? (<Message>{errorMsg[1]}</Message>): (
-                  error === 2 ? (<Message>{errorMsg[2]}</Message>) : (
-                    error === 3 ? (<Message>{errorMsg[3]}</Message>) : (
-                      error === 4 ? (<Message>{errorMsg[4]}</Message>) :
-                        error === 5 ? (<Message>{errorMsg[5]}</Message>) : (
-                          error === 0 ? (<Redirect to="/login" />) : null
-                        )
-                    )
-                  )
-                )}
+                {/* 이미 회원가입이 되어있는 메일일 경우, username이 존재할 경우 메세지 출력 */}
               </MsgDiv>
               <ButtonDiv>
                 <Button type="button" onClick={handleSubmit}>회원가입</Button>
