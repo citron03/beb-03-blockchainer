534c6370d5d0dd45e26bd4af2aeeae4943c164a9
BEB03 <cpt9136@gmail.com>
2022-04-20 13:55:19 +0900

    test

---
+++ server/models/post.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define(
+    "post",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      writer: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      charset: "utf8",
+      collate: "utf8_general_ci",
+    }
+  );
+  Post.associate = (db) => {
+    db.Post.hasMany(db.Comment, { foreignKey: "post_id", sourceKey: "id" });
+    db.Post.belongsTo(db.User, { foreignKey: "writer", targetKey: "id" });
+  };
+  return Post;
+};
