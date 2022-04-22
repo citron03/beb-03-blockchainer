1076a13bfea0dbd366ffd9123501dfe63d9dda7f
citron03 <cchan0617@gmail.com>
2022-04-22 15:21:30 +0900

    Home 페이지 newest post

--- client/src/components/home/NewsPost.jsx
+++ client/src/components/home/NewsPost.jsx
@@ -1,25 +1,48 @@
 import styled from "styled-components";
 import getPageCount from "../content/useData/getPageCount";
 import axios from "axios";
+import { useEffect, useState } from 'react';
 
 const fetchPost = async (id) => {
-    const getLastPosId = getPageCount();
-    console.log(getLastPosId);
-    const getPost = await axios.get(`http://localhost:4000/page/content/${id}`);
-    return getPost;
+    const getLastPosId = await getPageCount();
+    const getPost = await axios.get(`http://localhost:4000/page/content/${getLastPosId}`);
+    return getPost.data.data;
 }
 
 const NewsPostContainer = styled.div`
     flex: 1 1 0;
     margin: 1rem;
     background-color: white;
+    overflow: hidden;
+`
+const Paragraph = styled.p`
+    max-width: 70vw;
+    white-space: pre-line;
+    word-wrap: break-word;
+    padding-left: 1rem;
+    padding-right: 1rem;
 `
 
 const NewsPost = () => {
-    fetchPost();
+
+    const [newestPost, setNewestPost] = useState({});
+
+    useEffect(() => {
+        fetchPost()
+            .then(el => setNewestPost(el))
+            .catch(err => console.log(err));
+    }, [])
+
     return (
     <NewsPostContainer>
-        Hello
+        <p style={{padding: "1rem"}}>Newest Post</p>
+        {newestPost.id ? 
+            <>
+                <span style={{padding: "1rem"}}>{newestPost.title}</span>
+                <span style={{padding: "1rem"}}>작성자 : {newestPost.writer}</span>
+                <Paragraph>{newestPost.content}</Paragraph>
+            </>
+        : null}
     </NewsPostContainer>);
 }
 
