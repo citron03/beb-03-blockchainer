f58e73c06d5a11c9011df687e43c6e87b15f9623
Jeehee77 <gnarled47@gmail.com>
2022-04-22 16:07:49 +0900

    Redux Toolkit 적용

---
+++ client/src/Reducers/index.js
@@ -0,0 +1,7 @@
+import tokenReducer from './tokenReducer.js';
+
+const rootReducer = {
+    token: tokenReducer
+}
+
+export default rootReducer;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './Reducers'
+
+const store = configureStore({ reducer: rootReducer })
 
 ReactDOM.render(
+  <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
