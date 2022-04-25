948134f2c0db83d4228b450f394898a31335f314
Gidonge <gidong41@gmail.com>
2022-04-25 15:33:22 +0900

    mypage 구현 테스트중 ...

---
+++ server/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models');
+require('dotenv').config();
+
+router.get('/post/:username', async (req, res) => {
+  console.log(res);
+});
