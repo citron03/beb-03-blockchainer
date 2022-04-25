842cef5c78a4e06e48b9f805bfd50b553ac13df5
Gidonge <gidong41@gmail.com>
2022-04-25 15:16:38 +0900

    병합

--- server/app.js
+++ server/app.js
@@ -2,37 +2,39 @@ const express = require('express');
 const app = express();
 const db = require('./models');
 const port = 4000;
-<<<<<<< HEAD
-const cors = require("cors");
 
-const accountRouter = require("./routes/account");
-const contentRouter = require("./routes/content");
-const commentRouter = require("./routes/comment");
-const serverRouter = require("./routes/server");
-const pageRouter = require("./routes/page");
-const authRouter = require("./routes/auth");
-
-app.use(cors());
-app.use(express.json());
-app.use("/account", accountRouter);
-app.use("/content", contentRouter);
-app.use("/comment", commentRouter);
-app.use("/server", serverRouter);
-app.use("/page", pageRouter);
-app.use("/auth", authRouter);
-=======
 const cors = require('cors');
 
 const accountRouter = require('./routes/account');
+const contentRouter = require('./routes/content');
+const commentRouter = require('./routes/comment');
+const serverRouter = require('./routes/server');
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 
 app.use(cors());
 app.use(express.json());
 app.use('/account', accountRouter);
+app.use('/content', contentRouter);
+app.use('/comment', commentRouter);
+app.use('/server', serverRouter);
 app.use('/page', pageRouter);
 app.use('/auth', authRouter);
->>>>>>> bbed3b1 (병합전)
+const cors = require('cors');
+
+const accountRouter = require('./routes/account');
+const contentRouter = require('./routes/content');
+const commentRouter = require('./routes/comment');
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+
+app.use(cors());
+app.use(express.json());
+app.use('/account', accountRouter);
+app.use('/content', contentRouter);
+app.use('/comment', commentRouter);
+app.use('/page', pageRouter);
+app.use('/auth', authRouter);
 
 db.sequelize
   .sync({ force: true })
