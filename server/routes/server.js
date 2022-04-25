ad2a4fead05d1cd58724b64deceb8a5cf32822e9
BEB03 <cpt9136@gmail.com>
2022-04-25 15:24:29 +0900

    fix bug

--- server/routes/server.js
+++ server/routes/server.js
@@ -1,6 +1,9 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models");
 
-// router.post("/createaccount", async (req, res) => {});
+router.post("/createaccount", async (req, res) => {});
 
-// router.post("/", async (req, res) => {});
+router.post("/ethfaucet", async (req, res) => {});
+
+module.exports = router;
